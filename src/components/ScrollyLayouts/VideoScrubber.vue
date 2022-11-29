738f13d9f0df29fa56a68bb0e676f721df503bac
Moritz Riede <moritz@mriede.com>
2022-11-29 16:08:34 +0100

    feat: create a new scrubber component which can be reused in sticky side layouts

---
+++ src/components/ScrollyLayouts/VideoScrubber.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="fill-width fill-height">
+    <v-fade-transition>
+      <div style="position: relative" v-show="progress >= 0 && progress <= 100">
+        <video
+          v-if="baseUrl.includes('.mp4')"
+          ref="scrubVideo"
+          :src="baseUrl"
+          width="100%"
+          muted
+        />
+        
+        <canvas
+          v-else
+          ref="hero"
+          style="
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            pointer-events: none;
+            z-index: -1;
+          "
+        />
+      </div>
+    </v-fade-transition>
+    <v-container v-if="scrubConfig">
+      <v-col cols="6">
+        <article
+          class="black--text"
+          style="height: 1000px; padding: 400px 50px 300px 50px"
+          v-html="parseMarkdown(scrubConfig.content)"
+        />
+      </v-col>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+
+export default {
+  props: {
+    progress: Number,
+    baseUrl: String,
+    small: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: () => ({
+    frameCount: 134,
+    imageBase:
+      "https://codesandbox-scrollytelling-demo.s3.eu-central-1.amazonaws.com",
+    img: null,
+    context: null,
+    scrubConfig: null,
+  }),
+  computed: {
+    shouldRenderToCanvas () {
+      return !this.baseUrl.includes('.mp4');
+    }
+  },
+  mounted() {
+    if (this.shouldRenderToCanvas) {
+      this.setupCanvasRendering();
+    } else {
+      this.setupVideoRendering();
+    }
+  },
+  methods: {
+    currentFrame(index) {
+      return `${this.baseUrl}/${index.toString().padStart(4, "0")}.jpg`;
+    },
+    preloadImages() {
+      for (let i = 1; i < this.frameCount; i++) {
+        const img = new Image();
+        img.src = this.currentFrame(i);
+      }
+    },
+    parseMarkdown(input) {
+      return marked.parse(input).replace('<a', '<a target="_blank" ');
+    },
+    setupVideoRendering() {
+      document.getElementById('scroll-target').addEventListener('scroll' , e => {
+        
+      });
+    },
+    setupCanvasRendering() {
+      fetch(`${this.baseUrl}/scrub.json`)
+        .then((response) => {
+          response.json()
+            .then(json => {
+              this.scrubConfig = json;
+            })
+        })
+      const canvas = this.$refs.hero;
+      this.context = canvas.getContext("2d");
+
+      this.img = new Image();
+      this.img.src = this.currentFrame(1);
+      canvas.width = 1920;
+      canvas.height = 1080;
+      this.img.onload = () => {
+        this.context.drawImage(this.img, 0, 0, window.innerWidth, window.innerHeight);
+      };
+
+      this.preloadImages();
+    },
+  },
+  watch: {
+    progress(newProgress) {
+      if (newProgress >= 0 && newProgress <= 100) {
+        if (this.shouldRenderToCanvas) {
+          const newIndex = Math.floor((newProgress * this.frameCount) / 100) + 1;
+          this.img.src = this.currentFrame(newIndex);
+          this.context.drawImage(this.img, 0, 0, window.innerWidth, window.innerHeight);
+        } else {
+          let video = this.$refs.scrubVideo;
+          video.onloadedmetadata = function() {
+            console.log(`scroll!  duration: ${video.duration}, progress: ${this.progress} . ----- total: ${video.duration * (this.progress / 100)}`);
+            video.currentTime = video.duration * (this.progress / 100);
+          };
+        }
+      }
+    },
+  },
+};
+</script>

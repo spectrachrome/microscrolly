2d85d37dc21f5f0694f3c6d1af99fc750ebc5540
Moritz Riede <moritz@mriede.com>
2022-12-14 12:39:28 +0100

    fix: make sure everything using Vuetify lives within a v-app

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,60 @@
 <template>
-  <v-container
-    id="scroll-target"
-    style="
-      height: 100vh;
-      max-width: 100%;
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      padding: 0;
-    "
-    class="overflow-y-auto mx-0"
-    v-scroll.self="onScroll"
-  >
-    <template v-for="(item, index) in items">
-      <ImageWithTextOverlay
-        v-if="item[0].width === 4 && item[0].image"
-        :key="index"
-        :item="item"
-        :index="index"
-        :progress="progress[index] || 0"
-      />
-      <FullWidthBlock
-        v-else-if="item[0].width === 4
-          && item[0].text
-          || item[0].video"
-        :key="index"
-        :item="item[0]"
-      />
-      <StickyRight
-        v-else-if="item[0].width === 1"
-        :key="index"
-        :item="item"
-        :index="index"
-        :progress="progress[index]"
-      />
-      <StickyLeft
-        v-else-if="item[0].width === 3"
-        :key="index"
-        :item="item"
-        :index="index"
-        :progress="progress[index]"
-      />
-      <VideoScrubbingFullWidth
-        v-else-if="item[0].scrub"
-        :key="index"
-        :progress="progress[index]"
-        :base-url="item[0].scrub"
-      />
-    </template>
-    <div id="scrolly-bottom-nav" />
-    <div id="scrolly-footer" />
-  </v-container>
+  <v-app>
+    <v-container
+      id="scroll-target"
+      style="
+        height: 100vh;
+        max-width: 100%;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 0;
+      "
+      class="overflow-y-auto mx-0"
+      v-scroll.self="onScroll"
+    >
+      <template v-for="(item, index) in items">
+        <ImageWithTextOverlay
+          v-if="item[0].width === 4 && item[0].image"
+          :key="index"
+          :item="item"
+          :index="index"
+          :progress="progress[index] || 0"
+        />
+        <FullWidthBlock
+          v-else-if="item[0].width === 4
+            && item[0].text
+            || item[0].video"
+          :key="index"
+          :item="item[0]"
+        />
+        <StickyRight
+          v-else-if="item[0].width === 1"
+          :key="index"
+          :item="item"
+          :index="index"
+          :progress="progress[index]"
+        />
+        <StickyLeft
+          v-else-if="item[0].width === 3"
+          :key="index"
+          :item="item"
+          :index="index"
+          :progress="progress[index]"
+        />
+        <VideoScrubbingFullWidth
+          v-else-if="item[0].scrub"
+          :key="index"
+          :progress="progress[index]"
+          :base-url="item[0].scrub"
+        />
+      </template>
+      <div id="scrolly-bottom-nav" />
+      <div id="scrolly-footer" />
+    </v-container>
+  </v-app>
 </template>
 
 <script>

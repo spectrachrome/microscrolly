b91230833072e654a13f4d0777106fdd350d58d2
Moritz Riede <moritz@mriede.com>
2023-03-07 15:18:53 +0100

    refactor: remove useless redundancy

--- src/components/ScrollyLayouts/StickyRight.vue
+++ src/components/ScrollyLayouts/StickyRight.vue
@@ -7,109 +7,19 @@
         </article>
       </v-col>
       <v-col cols="8" style="z-index: 0">
-        <v-fade-transition>
-          <figure v-show="progress >= -50 && progress <= 150">
-            <VideoScrubber
-              v-if="item[1].scrub"
-              :progress="progress"
-              :base-url="item[1].scrub"
-              small
-            />
-
-            <video v-else-if="item[1].video && !item[1].autoplay" controls>
-              <source :src="item[1].video" type="video/mp4" />
-            </video>
-
-            <video
-              v-else-if="item[1].video && item[1].autoplay"
-              ref="autoplayVideo"
-              muted
-              playsinline
-              autoplay
-              loop
-            >
-              <source :src="item[1].video" type="video/mp4" />
-            </video>
-
-            <ImageCompare
-              v-else-if="item[1].compare"
-              :compare="item[1].compare"
-            />
-
-            <v-img v-else-if="item[1].image" :src="item[1].image" contain />
-
-            <iframe
-              v-else-if="item[1].iframe"
-              :src="item[1].iframe"
-              width="800px"
-              height="500px"
-              frameBorder="0"
-              scroll="no"
-              style="overflow: hidden"
-            ></iframe>
-
-            <TextSection v-else :text="item[1].text" />
-
-            <!-- Progress display for debugging -->
-            <!-- <span class="white--text pa-2" style="position: absolute; left: 0">
-              {{ Math.round(progress) || 0 }}%
-            </span> -->
-          </figure>
-        </v-fade-transition>
+        <MediaContainer
+          :item="item[1]"
+          :progress="progress"
+        />
       </v-col>
     </v-row>
 
     <v-col v-else justify="center">
       <div style="z-index: 0">
-        <v-fade-transition>
-          <figure v-show="progress >= -50 && progress <= 150">
-            <VideoScrubber
-              v-if="item[1].scrub"
-              :progress="progress"
-              :base-url="item[1].scrub"
-              small
-            />
-
-            <video v-else-if="item[1].video && !item[1].autoplay" controls>
-              <source :src="item[1].video" type="video/mp4" />
-            </video>
-
-            <video
-              v-else-if="item[1].video && item[1].autoplay"
-              ref="autoplayVideo"
-              muted
-              playsinline
-              autoplay
-              loop
-            >
-              <source :src="item[1].video" type="video/mp4" />
-            </video>
-
-            <ImageCompare
-              v-else-if="item[1].compare"
-              :compare="item[1].compare"
-            />
-
-            <v-img v-else-if="item[1].image" :src="item[1].image" contain />
-
-            <iframe
-              v-else-if="item[1].iframe"
-              :src="item[1].iframe"
-              width="800px"
-              height="500px"
-              frameBorder="0"
-              scroll="no"
-              style="overflow: hidden"
-            ></iframe>
-
-            <TextSection v-else :text="item[1].text" />
-
-            <!-- Progress display for debugging -->
-            <!-- <span class="white--text pa-2" style="position: absolute; left: 0">
-              {{ Math.round(progress) || 0 }}%
-            </span> -->
-          </figure>
-        </v-fade-transition>
+        <MediaContainer
+          :item="item[1]"
+          :progress="progress"
+        />
       </div>
       <div style="z-index: 0">
         <article style="padding: 800px 0">
@@ -122,17 +32,13 @@
 
 <script>
 import TextSection from "../TextSection.vue";
-import VideoScrubber from "./VideoScrubber.vue";
-import autoplayVideo from "../../mixins/autoplayVideo";
-import ImageCompare from "./ImageCompare.vue";
+import MediaContainer from "./MediaContainer.vue";
 
 export default {
   components: {
     TextSection,
-    VideoScrubber,
-    ImageCompare,
+    MediaContainer,
   },
-  mixins: [autoplayVideo],
   props: {
     item: Array,
     index: Number,
@@ -142,51 +48,5 @@ export default {
 </script>
 
 <style scoped>
-figure {
-  position: sticky;
-}
 
-/* smAndDown */
-@media only screen and (max-width: 959px) {
-  figure {
-    top: 120px !important;
-    height: 30vh;
-  }
-
-  :deep(figure iframe),
-  figure img,
-  :deep(figure .v-image),
-  figure video {
-    position: fixed;
-    top: 0;
-    width: 100%;
-  }
-}
-
-/* mdAndUp */
-@media only screen and (min-width: 960px) {
-  figure {
-    top: 50%;
-  }
-
-  :deep(figure iframe),
-  figure img,
-  :deep(figure .v-image),
-  figure video {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-25%, -50%);
-    max-height: 100vh;
-    max-width: 100%;
-  }
-}
-
-:deep(p) {
-  margin-bottom: 200px;
-}
-
-:deep(p:last-child) {
-  margin-bottom: 16px;
-}
 </style>
